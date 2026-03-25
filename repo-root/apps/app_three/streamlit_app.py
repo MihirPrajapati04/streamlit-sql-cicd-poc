bec77d14284d936e3cab0e1eab5ee2e83a3ba288
MihirPrajapati04 <prajapatimihir685@gmail.com>
2026-03-25 11:33:27 +0530

    chnaged all the apps

--- repo-root/apps/app_three/streamlit_app.py
+++ repo-root/apps/app_three/streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 
 st.title("App Two")
 st.write("Successfully deployed via CI/CD!")
+st.write("This app uses the CONTAINER runtime. Check out app_one for an example of WAREHOUSE runtime.")
--- repo-root/apps/app_two/streamlit_app.py
+++ repo-root/apps/app_two/streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 
 st.title("App Three")
 st.write("Successfully deployed via CI/CD!")
+st.write("This app uses the WAREHOUSE runtime. Check out app_one for an example of WAREHOUSE runtime.")
