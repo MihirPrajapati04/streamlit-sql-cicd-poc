be848d760d1e63e030e61032388f052382de0992
MihirPrajapati04 <prajapatimihir685@gmail.com>
2026-03-25 12:19:43 +0530

    changed

--- apps/app_three/streamlit_app.py
+++ apps/app_three/streamlit_app.py
@@ -1,6 +1,6 @@
-# apps/app_two/streamlit_app.py
+# apps/app_three/streamlit_app.py
 import streamlit as st
 
-st.title("App Two")
+st.title("App Three")
 st.write("Successfully deployed via CI/CD!")
-st.write("This app uses the CONTAINER runtime. Check out app_one for an example of WAREHOUSE runtime.")
+st.write("This app uses the WAREHOUSE runtime. Check out app_one for an example of WAREHOUSE runtime.")
--- apps/app_two/streamlit_app.py
+++ apps/app_two/streamlit_app.py
@@ -1,6 +1,6 @@
-# apps/app_three/streamlit_app.py
+# apps/app_two/streamlit_app.py
 import streamlit as st
 
-st.title("App Three")
+st.title("App Two")
 st.write("Successfully deployed via CI/CD!")
-st.write("This app uses the WAREHOUSE runtime. Check out app_one for an example of WAREHOUSE runtime.")
+st.write("This app uses the CONTAINER runtime. Check out app_one for an example of WAREHOUSE runtime.")
