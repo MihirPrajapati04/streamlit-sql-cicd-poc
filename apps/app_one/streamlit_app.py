948619a1df1c9bbe895ed7762d8ccf2ce18316f4
MihirPrajapati04 <prajapatimihir685@gmail.com>
2026-03-25 16:20:00 +0530

    changed

--- apps/app_one/streamlit_app.py
+++ apps/app_one/streamlit_app.py
@@ -4,6 +4,7 @@ from faker import Faker
 fake = Faker()
 
 st.title("App Two — pyproject.toml Test")
+st.write("This app uses the wrhs runtime. ")
 
 if st.button("Generate Fake Company"):
     st.write(fake.company())
