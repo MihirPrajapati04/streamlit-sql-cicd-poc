2d0ae4dea7cc946eb235aa1e9b262252459f32fb
MihirPrajapati04 <prajapatimihir685@gmail.com>
2026-03-25 11:23:16 +0530

    chnaged

--- repo-root/apps/app_one/streamlit_app.py
+++ repo-root/apps/app_one/streamlit_app.py
@@ -5,3 +5,4 @@ import streamlit as st
 
 st.title("App One")
 st.write("Successfully deployed via CI/CD!")
+st.write("This app uses the WAREHOUSE runtime. Check out app_two for an example of CONTAINER runtime.")
