0b3d5e083c8d78cbf828249e246f1fe1a0469b47
MihirPrajapati04 <prajapatimihir685@gmail.com>
2026-03-27 12:51:56 +0530

    chanegd for prod testing 3

--- apps/app_one/streamlit_app.py
+++ apps/app_one/streamlit_app.py
@@ -10,6 +10,11 @@ st.write(f"SNOWFLAKE_ENV: {os.environ['SNOWFLAKE_ENV']}")
 st.write("This app uses the wrhs runtime. ")
 st.write("It has a pyproject.toml file for dependencies, but no environment.yml.")
 st.write("This tests that the deployment process correctly handles pyproject.toml without environment.yml for a warehouse runtime app.")
+st.write(f"SNOWFLAKE_ENV_ID: {os.environ['SNOWFLAKE_ENV_ID']}")
+st.write(f"SNOWFLAKE_ENV: {os.environ['SNOWFLAKE_ENV']}")
+st.write("This app uses the wrhs runtime. ")
+st.write("It has a pyproject.toml file for dependencies, but no environment.yml.")
+st.write("This tests that the deployment process correctly handles pyproject.toml without environment.yml for a warehouse runtime app.")
 
 if st.button("Generate Fake Company"):
     st.write(fake.company())
