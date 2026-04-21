efcc71311d7080f7cb9d156f9ef96ddbdc3e847e
MihirPrajapati04 <prajapatimihir685@gmail.com>
2026-04-21 18:19:33 +0530

    created app four

---
+++ apps/app_four/streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from faker import Faker
+import os 
+
+fake = Faker()
+
+st.title("App One — pyproject.toml Test")
+st.write(f"SNOWFLAKE_ENV_ID: {os.environ['SNOWFLAKE_ENV_ID']}")
+st.write(f"SNOWFLAKE_ENV: {os.environ['SNOWFLAKE_ENV']}")
+st.write(f"SNOWFLAKE_ACCOUNT: {os.environ['SNOWFLAKE_ACCOUNT']}")
+st.write(f"SNOWFLAKE_USER: {os.environ['SNOWFLAKE_USER']}")
+
+
+
+st.write("This app uses the wrhs runtime. ")
+st.write("It has a pyproject.toml file for dependencies, but no environment.yml.")
+st.write("This tests that the deployment process correctly handles pyproject.toml without environment.yml for a warehouse runtime app.")
+
+if st.button("Generate Fake Company"):
+    st.write(fake.company())
+    st.write(fake.catch_phrase())
+    st.write(fake.bs())
+    st.write(fake.address())
